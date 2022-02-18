56b9e90abf0c484dbe28bb7dcb39064899e62454
ryo-imai-bit <mayoimaimai9@gmail.com>
2022-02-18 19:53:54 +0900

    add extensions

--- .vscode/install.sh
+++ .vscode/install.sh
@@ -7,8 +7,8 @@ VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 rm "$VSCODE_SETTING_DIR/settings.json"
 ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-#rm "$VSCODE_SETTING_DIR/keybindings.json"
-#ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 # install extention
 cat $SCRIPT_DIR/extensions | while read line
--- .zshrc
+++ .zshrc
@@ -26,9 +26,9 @@ if type "opam" > /dev/null 2>&1; then
     # Ocaml
     eval `opam env`
 fi
-if [ -e ~/flutter/bin ]; then
-    # Flutter
-    export PATH="$PATH:$HOME/flutter/bin"
+if type "rbenv" > /dev/null 2>&1; then
+    # Ruby
+    eval "$(rbenv init - zsh)"
 fi
 
 # ghq cd project
